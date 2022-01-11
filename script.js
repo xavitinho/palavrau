dc6b1c29c035d217b17cd40ca283799de4941b25
xaax <85655796+xavitinho@users.noreply.github.com>
2022-01-11 16:11:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let palavras = ''
 
 var palavra = false
 var chance = 0
+var tabuleiroletras = []
 var tabuleiro = []
 
 const letras = [
@@ -10,17 +11,17 @@ const letras = [
   ["z", "x", "c", "v", "b", "n", "m"]
 ]
 
-let iniciotxt = 
-`<p>Você tem 6 chances para descobrir a palavra VRAU!</p>
+let textoajuda =
+  `<p>Você tem 6 chances para descobrir a palavra VRAU!</p>
 <p>Após cada tentativa, a cor de cada letra vai mudar para mostrar o quão próximo você esttá da palavra certa</p>
 <br><br>
 <table id="exemplo1" class="table is-narrow is-family-monospace">
   <tr>
-    <td class="td"> <div class="tag is-black   is-size-3"> t </div></td>
-    <td class="td"> <div class="tag is-black   is-size-3"> e </div></td>
-    <td class="td"> <div class="tag is-warning is-size-3"> s </div></td>
-    <td class="td"> <div class="tag is-black   is-size-3"> t </div></td>
-    <td class="td"> <div class="tag is-black   is-size-3"> e </div></td>
+    <td class="td"> <div class="tag is-black   is-size-3"> T </div></td>
+    <td class="td"> <div class="tag is-black   is-size-3"> E </div></td>
+    <td class="td"> <div class="tag is-warning is-size-3"> S </div></td>
+    <td class="td"> <div class="tag is-black   is-size-3"> T </div></td>
+    <td class="td"> <div class="tag is-black   is-size-3"> E </div></td>
   </tr>
 </table>
 <p>A letra <strong>S</strong> está na palavra mas em outra posição</p>
@@ -29,10 +30,10 @@ let iniciotxt =
 <table id="exemplo1" class="table is-narrow is-family-monospace">
   <tr>
     <td class="td"> <div class="tag is-black   is-size-3"> J </div></td>
-    <td class="td"> <div class="tag is-black   is-size-3"> e </div></td>
-    <td class="td"> <div class="tag is-black   is-size-3"> s </div></td>
-    <td class="td"> <div class="tag is-primary is-size-3"> u </div></td>
-    <td class="td"> <div class="tag is-black   is-size-3"> s </div></td>
+    <td class="td"> <div class="tag is-black   is-size-3"> E </div></td>
+    <td class="td"> <div class="tag is-black   is-size-3"> S </div></td>
+    <td class="td"> <div class="tag is-primary is-size-3"> U </div></td>
+    <td class="td"> <div class="tag is-black   is-size-3"> S </div></td>
   </tr>
 </table>
 <p>A letra <strong>U</strong> está na palavra e na posição certa.</p>
@@ -40,48 +41,56 @@ let iniciotxt =
    
 <table id="exemplo1" class="table is-narrow is-family-monospace">
   <tr>
-  <td class="td"> <div class="tag is-warning is-size-3"> r </div></td>
-    <td class="td"> <div class="tag is-warning is-size-3"> e </div></td>
-    <td class="td"> <div class="tag is-warning is-size-3"> s </div></td>
-    <td class="td"> <div class="tag is-warning is-size-3"> t </div></td>
-    <td class="td"> <div class="tag is-black   is-size-3"> o </div></td>
+    <td class="td"> <div class="tag is-warning is-size-3"> R </div></td>
+    <td class="td"> <div class="tag is-warning is-size-3"> E </div></td>
+    <td class="td"> <div class="tag is-warning is-size-3"> S </div></td>
+    <td class="td"> <div class="tag is-warning is-size-3"> T </div></td>
+    <td class="td"> <div class="tag is-black   is-size-3"> O </div></td>
   </tr>
 </table>
 <p>A letra <strong>O</strong> não está na palavra em posição nenhuma.</p>`
 
-
 document.addEventListener('keydown', (event) => {
   let tecla = event.key.toLowerCase()
-  if (letras[0].includes(tecla) || letras[1].includes(tecla) || letras[2].includes(tecla)){
+  if (letras[0].includes(tecla) || letras[1].includes(tecla) || letras[2].includes(tecla)) {
     letra(tecla)
   } else {
-    switch(tecla){
+    switch (tecla) {
       case 'backspace':
         limpar()
-      break
+        break
       case 'enter':
         verificar()
-      break
+        break
       case 'escape':
         closemodal()
-      break
+        break
     }
   }
 }, false);
 
+
 main()
-alerta(iniciotxt)
+alerta(textoajuda)
 
 async function main() {
   closemodal()
   chance = 0
+  tabuleiroletras = [
+    [false, false, false, false, false],
+    [false, false, false, false, false],
+    [false, false, false, false, false],
+    [false, false, false, false, false],
+    [false, false, false, false, false],
+    [false, false, false, false, false]
+  ]
   tabuleiro = [
-    [false, false, false, false, false, false],
-    [false, false, false, false, false, false],
-    [false, false, false, false, false, false],
-    [false, false, false, false, false, false],
-    [false, false, false, false, false, false],
-    [false, false, false, false, false, false]
+    [false, false, false, false, false],
+    [false, false, false, false, false],
+    [false, false, false, false, false],
+    [false, false, false, false, false],
+    [false, false, false, false, false],
+    [false, false, false, false, false]
   ]
 
   await fetch('https://raw.githubusercontent.com/xavitinho/palavrau/main/todas_as_palavras.json')
@@ -105,7 +114,7 @@ function iniciatabuleiro() {
   let code = ''
   for (let i = 0; i < 6; i++) {
     code += `\n<tr id='p${i}'>`
-    for (let j = 1; j < 6; j++) {
+    for (let j = 0; j < 5; j++) {
       code +=
         `\n
     <td class="td">
@@ -118,12 +127,12 @@ function iniciatabuleiro() {
 }
 
 function letra(l) {
-  let j = 1
-  while (tabuleiro[chance][j]) {
+  let j = 0
+  while (tabuleiroletras[chance][j]) {
     j++;
   }
-  if (j < 6) {
-    tabuleiro[chance][j] = l
+  if (j < 5) {
+    tabuleiroletras[chance][j] = l
     let show = document.getElementById(`p${chance}l${j}`)
     show.innerText = l.toUpperCase()
   }
@@ -131,49 +140,48 @@ function letra(l) {
 
 function verificar() {
   let skip = false;
-  for (let l = 1; l < 6; l++) {
-    if (!tabuleiro[chance][l]) {
+  for (let l = 0; l < 5; l++) {
+    if (!tabuleiroletras[chance][l]) {
       alerta('palavra incompleta!')
       skip = true;
     }
   }
   if (!skip) {
     let chute = ''
-    for (let j = 1; j < 6; j++) {
-      chute += tabuleiro[chance][j]
+    for (let j = 0; j < 5; j++) {
+      chute += tabuleiroletras[chance][j]
     }
-    console.log(chute)
     if (palavras.validas[chute] || palavras.certas[chute]) {
       let ls = palavra.split('')
       for (let j = 0; j < 5; j++) {
-        console.log(ls[j])
-        if (ls[j] !== 'ç') {
-          ls[j] = ls[j].normalize('NFD').replace(/[\u0300-\u036f]/g, "")
-        }
-        console.log(ls[j])
-        let show = document.getElementById(`p${chance}l${j + 1}`)
-        let tecla = document.getElementById(tabuleiro[chance][j + 1])
-        if (ls[j] === tabuleiro[chance][j + 1]) {
+        ls[j] = ls[j].normalize('NFD').replace(/[\u0300-\u036f]/g, "")
+        let show = document.getElementById(`p${chance}l${j}`)
+        let tecla = document.getElementById(tabuleiroletras[chance][j])
+        if (ls[j] === tabuleiroletras[chance][j]) {
           show.className = show.className.replace('is-dark', 'is-primary')
           tecla.className = tecla.className.replace('is-light is-outlined', 'is-primary')
+          tabuleiro[chance][j] = 2
         } else {
-          if (ls.includes(tabuleiro[chance][j + 1])) {
+          if (ls.includes(tabuleiroletras[chance][j])) {
             show.className = show.className.replace('is-dark', 'is-warning')
             tecla.className = tecla.className.replace('is-light is-outlined', 'is-primary')
+            tabuleiro[chance][j] = 1
           }
           else {
             show.className = show.className.replace('is-dark', 'is-black')
             tecla.className = tecla.className.replace('is-light is-outlined', 'is-black')
+            tabuleiro[chance][j] = 0
           }
         }
       }
-      if (palavra === chute) {
+      if (chute === palavra.normalize('NFD').replace(/[\u0300-\u036f]/g, "")) {
+        chance++
         fim(true)
-      }
-      chance++
-      console.log(chance)
-      if (chance > 5) {
-        fim(false)
+      } else {
+        chance++
+        if (chance > 5) {
+          fim(false)
+        }
       }
     } else {
       alerta('palavra não é valida')
@@ -182,23 +190,130 @@ function verificar() {
 }
 
 function limpar() {
-  for (let l = 1; l < 6; l++) {
-    tabuleiro[chance][l] = false
+  for (let l = 0; l < 5; l++) {
+    tabuleiroletras[chance][l] = false
     let show = document.getElementById(`p${chance}l${l}`)
     show.innerHTML = '<br>'
   }
 }
 
+function calcpt() {
+  let sorte = 0
+  let habilidade = 0
+
+  letrasdesc = []
+  let arrayp = palavra.split('')
+
+  for (let p = 0; p < chance - 1; p++) {
+    for (let l = 0; l < 5; l++) {
+      if (tabuleiroletras[p].includes(arrayp[l]) && !letrasdesc.includes(arrayp[l])) {
+        letrasdesc.push(arrayp[l])
+      }
+    }
+  }
+
+  let p = -1
+  while (sorte === 0) {
+    for (l of tabuleiro[p + 1]) {
+      sorte += l * 10
+    }
+    p++
+  }
+  sorte -= p * 13
+  if(chance>1) {
+    habilidade += (6 - chance/2) * 10
+  habilidade += (5 - letrasdesc.length/2) * 10
+  }
+  
+  habilidade -= habilidade * sorte/150
+
+  if (sorte < 0) { sorte = 0 }
+  if (sorte > 100) { sorte = 100 }
+  if (habilidade < 0) { habilidade = 0 }
+  if (habilidade > 100) { habilidade = 100 }
+
+  return ([sorte, habilidade])
+}
+
+function fazemoji() {
+  let emojistt = ''
+  let emojishtml = ''
+  for (let p = 0; p < chance; p++) {
+    for (let l = 0; l < 5; l++) {
+      switch (tabuleiro[p][l]){
+        case 0:
+          emojistt += '⬛'
+          emojishtml += '⬜ '
+          break
+        case 1:
+          emojistt += '🟧'
+          emojishtml += '🟧 '
+          break
+        case 2:
+          emojistt += '🟩'
+          emojishtml += '🟦 '
+          break
+      }
+    }
+    emojistt += '%0A'
+    emojishtml += '<br>'
+  }
+  return([emojistt, emojishtml])
+}
+
 function fim(vitoria) {
+  let pt = calcpt()
+  let arrayp = palavra.split('')
+  let emojis = fazemoji()
+  let textovitoria = `
+          <strong> PARABÉNS! Você venceu! </strong>
+        
+          <div class="palavrafinal">
+            <br>
+            <p> Palavra:</p>
+            <table id="exemplo1" class="table is-narrow is-family-monospace">
+              <tr>
+                <td class="td"> <div class="tag is-primary is-size-3"> ${arrayp[0].toUpperCase()} </div></td>
+                <td class="td"> <div class="tag is-primary is-size-3"> ${arrayp[1].toUpperCase()} </div></td>
+                <td class="td"> <div class="tag is-primary is-size-3"> ${arrayp[2].toUpperCase()} </div></td>
+                <td class="td"> <div class="tag is-primary is-size-3"> ${arrayp[3].toUpperCase()} </div></td>
+                <td class="td"> <div class="tag is-primary is-size-3"> ${arrayp[4].toUpperCase()} </div></td>
+              </tr>
+            </table>
+            <br>
+          </div>
+          <br><strong>compartilhe seu resultado:</strong><br>
+          <div class="tabuleirofinal">
+            <br> ${emojis[1]} <br>
+          </div>
+
+          <div class="barras">
+            <strong style="font-size:0.8em"> sorte: </strong>
+            <div class="barra">
+              <div id="sorte" style="height: 100%; width:${pt[0]}%; background-color:#d22;">
+                <strong>${pt[0]}%</strong>
+              </div>
+            </div>
+            <br><strong style="font-size:0.8em"> habilidade: </strong>
+            <div class="barra">
+              <div id="habilidade" style="height: 100%; width:${pt[1]}%; background-color:#2b2;">
+                <strong>${pt[1]}%</strong>
+              </div>
+            </div>
+            <br>
+            <br>
+          </div>
+
+          <div class="buttons">   
+            <button class="button is-fullwidth is-light is-outlined" onclick="main()">jogar novamente</button>
+            <a class="button is-fullwidth is-dark" href="https://twitter.com/compose/tweet?text=joguei+palaVRAU:%0A%0AA palavra era ${palavra.toUpperCase()}%0A${emojis[0]}%0Axavitinho.github.io/palavrau+de+@xavitinho+">compartilhar no twitter</a>
+          </div>`
+
   if (vitoria) {
-    alerta('VITORIA!!!!!!')
+    alerta(textovitoria)
   } else {
     alerta('tente de novo :(')
   }
-  let a = document.getElementById('alerta')
-  a.innerHTML += `<button class="button is-fullwidth is-light is-outlined" onclick="main()">jogar novamente</button>
-  <a class="button is-fullwidth is-dark" href="https://twitter.com/compose/tweet?text=joguem+palaVRAU+->+http://xavitinho.github.io/palavrau de @xavitinho
-+ ">COMPARTILHAR NO TWITTER</button>`
 }
 
 function alerta(texto) {
